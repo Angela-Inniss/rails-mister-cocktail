78f0ed2d5c9a0bee18ade9f9578b0b0f14b7665f
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-02-22 12:13:04 +0000

    added links to cocktails on index page

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,7 +1,7 @@
 class Dose < ApplicationRecord
   belongs_to :ingredient
   belongs_to :cocktail
-  validates :description, uniqueness: true, presence: true
+  validates :description, presence: true
   validates :cocktail, presence: true, uniqueness: { scope: :ingredient }
  end
   # a cocktail is present in a dose and is unique and a cocktail also has an
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,16 +15,16 @@ Dose.destroy_all
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
 # open url we want to extract information from
-puts information = open(url).read
+information = open(url).read
 # parse information and check in consile what the info looks like
 # this information is a hash
-puts ingredients = JSON.parse(information)
+ ingredients = JSON.parse(information)
 # extract information by iterating over the hash key which is "drinks" in this
 # case
 ingredients["drinks"].each do |ingredient|
 # then inside the hash look at the key so we can get the value of the ingredient
 # name
-  puts ingredient["strIngredient1"]
+  ingredient["strIngredient1"]
 # create the ingredient for the database using following syntax
   Ingredient.create(name: ingredient["strIngredient1"])
 end
@@ -37,5 +37,7 @@ Cocktail.create(name: "Angela's Delight")
 Cocktail.create(name: "Sex on the Beach")
 
 Cocktail.all.each do |cocktail|
-  Dose.create(description: "2cl", ingredient: Ingredient.first, cocktail: cocktail)
+  5.times do
+    dose = Dose.create(description: "2cl", ingredient: Ingredient.offset(rand(Ingredient.count)).first, cocktail: cocktail)
+  end
 end
