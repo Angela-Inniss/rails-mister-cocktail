0382cb3f849ab08884970bad40fbd83565df075a
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-02-22 14:41:46 +0000

    added doses controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,19 +1,30 @@
 class DosesController < ApplicationController
   def new
     @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
   def create
-    @dose = Dose.create(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
-    @dose.delete
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktails_path
   end
-end
 
 private
 
-def dose_params
-  params.require(:dose).permit(:description)
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
