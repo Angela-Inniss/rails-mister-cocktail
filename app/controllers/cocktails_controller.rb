d93915822dedab99ea92c4eecdfdbb091130af6b
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-02-23 15:23:57 +0000

    deleted all files related to articles and changed header fonts to google font Supermercado One

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -30,6 +30,13 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
+   def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+
   def cocktail_params
     params.require(:cocktail).permit(:name, :ingredient)
   end
