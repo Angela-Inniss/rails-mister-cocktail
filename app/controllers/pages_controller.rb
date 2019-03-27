fb49582849392ab46fd4ab049437582a36e31ada
Angela Inniss <Angela.inniss@hotmail.co.uk>
2019-03-27 22:23:43 +0000

    added pg search and some styling to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def home
-    @coktails = Cocktail.all
+
   end
 end
--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,15 @@
+
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
   validates :name, uniqueness: true, presence: true
   mount_uploader :photo, PhotoUploader
+
+  include PgSearch
+
+  pg_search_scope :search_by_name,
+    against: [:name],
+    using: {
+      tsearch: { prefix: true } # <-- now `superman batm` will return something!
+    }
 end
